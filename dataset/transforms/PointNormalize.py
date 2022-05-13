26f32bad884a9dd5747de4e51089a4e8039b6bee
Yu Geunhyeok <nda111@naver.com>
2022-05-13 10:47:04 +0900

    Update PointNormalize.py
    MinMax normalize

--- dataset/transforms/PointNormalize.py
+++ dataset/transforms/PointNormalize.py
@@ -3,14 +3,12 @@ import torch
 
 class PointNormalize:
     def __call__(self, x: torch.Tensor):
-        # centered = x - torch.mean(x, dim=0)
-        # out = centered / torch.max(torch.norm(centered, dim=1))
-        # return out
+        _min = torch.min(x, dim=1).values.view(-1, 1)
+        _max = torch.max(x, dim=1).values.view(-1, 1)
+        scale = _max - _min
 
-        # centered = x - torch.mean(x, dim=0)
-        # return centered / torch.max(centered)
-    
-        return x
+        out = (x - _min) * 2 / scale - 1
+        return out
 
     def __repr__(self):
         return f'{self.__class__.__name__}()'
