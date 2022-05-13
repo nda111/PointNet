4c39fbfe663d1d13dffc93130293fa73150eab3e
Yu Geunhyeok <nda111@naver.com>
2022-05-13 12:13:27 +0900

    Update FarthestPointSampling.py

--- dataset/transforms/FarthestPointSampling.py
+++ dataset/transforms/FarthestPointSampling.py
@@ -12,17 +12,16 @@ class FarthestPointSampling(Sampling):
 
     @staticmethod
     def sample_points(x: torch.Tensor, num_points: int, ndim: int, num_samples: int) -> torch.Tensor:
-        centroid_idx = torch.randint(low=0, high=num_points, size=(1,))
-        samples = []
-        for _ in range(num_samples):
-            centroid = x[centroid_idx].view(1, ndim)
-            x = torch.cat([x[:centroid_idx], x[centroid_idx + 1:]], dim=0)
-
-            distances = torch.norm(x - centroid, dim=0)
-            farthest_idx = torch.max(distances, dim=0).indices
-
-            samples.append(x[farthest_idx].view(1, ndim))
-            centroid_idx = farthest_idx
-        samples = torch.cat(samples, dim=0)
+        centroids = torch.zeros((num_samples,))
+        distance = torch.ones((num_points,)) * 1.0E+10
+        farthest = torch.randint(low=0, high=num_points, size=(1,))
+        for i in range(num_samples):
+            centroids[i] = farthest
+            centroid = x[farthest, :]
+            dist = torch.sum((x - centroid) ** 2, -1)
+            mask = dist < distance
+            distance[mask] = dist[mask]
+            farthest = torch.argmax(distance, -1)
+        samples = x[centroids.long()]
 
         return samples
