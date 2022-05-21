3b0e1ef30d2db284f58518ba483596ac81069b65
Yu Geunhyeok <nda111@naver.com>
2022-05-21 21:08:33 +0900

    Update TransformComparisonPlan.py

--- experiment/plan/TransformComparisonPlan.py
+++ experiment/plan/TransformComparisonPlan.py
@@ -24,17 +24,19 @@ class TransformComparisonPlan(Plan):
         trainer = ClassifierPlan(model, optimizer, self.train_dataset, self.test_dataset, paths[0], num_epochs=2)
         trainer.execute()
 
-        model_tnet = model
-        tnet = TNet(3)
-        model_tnet.base.set_input_transform(tnet)
-        optimizer_tnet = optim.Adam(tnet.parameters(), lr=1.0E-3)
-        trainer = ClassifierPlan(model_tnet, optimizer_tnet, self.train_dataset, self.test_dataset, paths[1], num_epochs=2)
-        trainer.execute()
-
         model_qnet = PointNetClassifier(40).load_state_dict(model.state_dict(), strict=True)
         qnet = QuaternionNet()
         model_qnet.base.set_input_transform(qnet)
         optimizer_qnet = optim.Adam(qnet.parameters(), lr=1.0E-3)
+
+        model_tnet = model
+        tnet = TNet(3)
+        model_tnet.base.set_input_transform(tnet)
+        optimizer_tnet = optim.Adam(tnet.parameters(), lr=1.0E-3)
+
+        trainer = ClassifierPlan(model_tnet, optimizer_tnet, self.train_dataset, self.test_dataset, paths[1], num_epochs=2)
+        trainer.execute()
+
         trainer = ClassifierPlan(model_qnet, optimizer_qnet, self.train_dataset, self.test_dataset, paths[2], num_epochs=2)
         trainer.execute()
 
